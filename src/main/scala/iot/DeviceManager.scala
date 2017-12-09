cdada1b655d1e3291ecf7efd2e3cb2e03de5f416
mxq <ready_man@126.com>
2017-12-09 22:41:47 +0800

    example

--- src/main/scala/iot/DeviceManager.scala
+++ src/main/scala/iot/DeviceManager.scala
@@ -1,11 +1,47 @@
 package iot
 
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
+
 /**
   * Created by Administrator on 2017/12/3.
   */
 object DeviceManager {
+  def props(): Props = Props(new DeviceManager)
+
   final case class RequestTrackDevice(groupId: String, deviceId: String)
 
   case object DeviceRegistered
 
+  case class QueryDeviceGroups(requestId: Int)
+  case class RespondDeviceGroupList(requestId: Int, ids: Set[String])
+}
+
+class DeviceManager extends Actor with ActorLogging {
+  var groupIdToActor = Map.empty[String, ActorRef]
+  var actorToGroupId = Map.empty[ActorRef, String]
+
+  override def preStart(): Unit = log.info("DeviceManager started")
+  override def postStop(): Unit = log.info("DeviceManager stopped")
+
+  import DeviceManager._
+  override def receive: Receive = {
+    case trackMsg @ RequestTrackDevice(groupId, _) =>
+      groupIdToActor.get(groupId) match {
+        case Some(ref) =>
+          ref forward trackMsg
+        case None =>
+          log.info("creating device group actor for {}", groupId)
+          val group = context.actorOf(DeviceGroup.props(groupId))
+          groupIdToActor += groupId -> group
+          actorToGroupId += group -> groupId
+          group forward trackMsg
+      }
+    case Terminated(groupActor) =>
+      val groupId = actorToGroupId(groupActor)
+      actorToGroupId -= groupActor
+      groupIdToActor -= groupId
+      log.info("Device Group actor for {} has been terminated", groupId)
+    case QueryDeviceGroups(requestId) =>
+      sender() ! RespondDeviceGroupList(requestId, groupIdToActor.keySet)
+  }
 }
--- src/test/scala/IotTester.scala
+++ src/test/scala/IotTester.scala
@@ -1,6 +1,6 @@
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, PoisonPill}
 import akka.testkit.{TestKit, TestProbe}
-import iot.{Device, DeviceGroup, DeviceManager}
+import iot.{Device, DeviceGroup, DeviceGroupQuery, DeviceManager}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
 import scala.concurrent.duration._
@@ -97,5 +97,113 @@ class IotTester(_system: ActorSystem)
 
     groupActor.tell(DeviceManager.RequestTrackDevice("wrongGroup", "device1"), probe.ref)
     probe.expectNoMsg(500 milliseconds)
+  }
+
+  "iot7" should "reply device list " in {
+    val probe = TestProbe()
+    val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+
+    val toShutdown = probe.lastSender
+    probe.watch(toShutdown)
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+
+    groupActor.tell(DeviceGroup.RequestDeviceList(requestId = 1), probe.ref)
+    probe.expectMsg(DeviceGroup.ReplyDeviceList(1, Set("device1", "device2")))
+
+    toShutdown.tell(PoisonPill, probe.ref)
+
+    probe.expectTerminated(toShutdown)
+    probe.awaitAssert{
+      groupActor.tell(DeviceGroup.RequestDeviceList(requestId = 2), probe.ref)
+      probe.expectMsg(DeviceGroup.ReplyDeviceList(2, Set("device2")))
+    }
+
+  }
+
+  "device group test" should "device group manager " in {
+    val probe = TestProbe()
+    val deviceManager = system.actorOf(DeviceManager.props())
+
+    import DeviceManager._
+    deviceManager.tell(RequestTrackDevice("group1", "device1"), probe.ref)
+    probe.expectMsg(DeviceRegistered)
+
+    deviceManager.tell(RequestTrackDevice("group1", "device2"), probe.ref)
+    probe.expectMsg(DeviceRegistered)
+
+    deviceManager.tell(RequestTrackDevice("group2", "device3"), probe.ref)
+    probe.expectMsg(DeviceRegistered)
+
+    deviceManager.tell(RequestTrackDevice("group2", "device4"), probe.ref)
+    probe.expectMsg(DeviceRegistered)
+
+
+    deviceManager.tell(QueryDeviceGroups(requestId = 1), probe.ref)
+    probe.expectMsg(RespondDeviceGroupList(1, Set("group1", "group2")))
+  }
+
+  "device group query " should "return temperature value for working devices " in {
+    val requester = TestProbe()
+
+    val device1 = TestProbe()
+    val device2 = TestProbe()
+
+    val queryActor = system.actorOf(DeviceGroupQuery.props(
+      actorToDeviceId = Map(device1.ref -> "device1", device2.ref -> "device2"),
+      requestId = 1,
+      requester = requester.ref,
+      timeout = 3.seconds
+    ))
+
+    device1.expectMsg(Device.ReadTemperature(requestId = 0))
+    device2.expectMsg(Device.ReadTemperature(requestId = 0))
+
+    queryActor.tell(Device.RespondTemperature(requestId = 0, Some(1.0)), device1.ref)
+    queryActor.tell(Device.RespondTemperature(requestId = 0, Some(2.0)), device2.ref)
+
+    requester.expectMsg(DeviceGroup.RespondAllTemperatures(
+      requestId = 1,
+      temperatures = Map(
+        "device1" -> DeviceGroup.Temperature(1.0),
+        "device2" -> DeviceGroup.Temperature(2.0)
+      )
+    ))
+  }
+
+  "final test" should "be able to collect temperatures from all active devices" in {
+    val probe = TestProbe()
+    val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    val deviceActor1 = probe.lastSender
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    val deviceActor2 = probe.lastSender
+
+    groupActor.tell(DeviceManager.RequestTrackDevice("group", "device3"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    val deviceActor3 = probe.lastSender
+
+    // Check that the device actors are working
+    deviceActor1.tell(Device.RecordTemperature(requestId = 0, 1.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 0))
+    deviceActor2.tell(Device.RecordTemperature(requestId = 1, 2.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+
+    groupActor.tell(DeviceGroup.RequestAllTemperatures(requestId = 0), probe.ref)
+
+    probe.expectMsg(DeviceGroup.RespondAllTemperatures(
+      requestId = 0,
+      temperatures = Map("device1" -> DeviceGroup.Temperature(1.0),
+                         "device2" -> DeviceGroup.Temperature(2.0),
+                         "device3" -> DeviceGroup.TemperatureNotAvailable)
+    ))
   }
 }
